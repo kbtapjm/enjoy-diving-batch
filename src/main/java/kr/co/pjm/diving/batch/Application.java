a83d74e6f339dad5726a937bbeac5fa574043219
kbtapjm <kbtapjm@gmail.com>
2019-02-15 10:55:04 +0900

    first commit

---
+++ src/main/java/kr/co/pjm/diving/batch/Application.java
@@ -0,0 +1,33 @@
+package kr.co.pjm.diving.batch;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@EnableBatchProcessing
+@SpringBootApplication
+public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @Bean("jasyptStringEncryptor")
+  public StringEncryptor stringEncryptor() {
+    PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+    SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+    config.setPassword("test");
+    config.setAlgorithm("PBEWithMD5AndDES");
+    config.setKeyObtentionIterations("1000");
+    config.setPoolSize("1");
+    config.setProviderName("SunJCE");
+    config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+    config.setStringOutputType("base64");
+    encryptor.setConfig(config); 
+
+    return encryptor;
+  }
+}
---
+++ src/test/java/kr/co/pjm/diving/batch/enjoydivingbatch/EnjoyDivingBatchApplicationTests.java
@@ -0,0 +1,17 @@
+package kr.co.pjm.diving.batch.enjoydivingbatch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EnjoyDivingBatchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
